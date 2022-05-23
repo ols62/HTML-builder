7579dd9aee30787f3cb5f3e80d8eed6144365e6a
Oleg S <oleg.sl62@mail.ru>
2022-05-24 01:27:40 +0300

    feat: finish task 06-build-page

---
+++ 06-build-page/index.js
@@ -0,0 +1,130 @@
+const path = require('path');
+const { copyFile, readdir, mkdir } = require('fs/promises');
+const fs = require('fs');
+
+const distdirname = path.join(__dirname, 'project-dist');
+const assetsdirname = path.join(__dirname, 'assets');
+const stylesdir = path.join(__dirname, 'styles');
+const distdir = path.join(__dirname, 'project-dist');
+const stylesfilename = path.join(distdir, 'style.css');
+const stylesfile = new fs.WriteStream(stylesfilename, 'utf8');
+let content = [];
+
+const createdist = async () => {
+  try {
+    await mkdir(distdirname, { recursive: true });
+    const adir = path.join(distdirname, 'assets');
+    await mkdir(adir, { recursive: true });
+    await copyassest(assetsdirname, adir);
+    const files = await readdir(assetsdirname, { withFileTypes: true });
+    for (const file of files) {
+      const dist = path.join(adir, file.name);
+      const src = path.join(assetsdirname, file.name);
+      await copyassest(src, dist);
+    }
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const copyassest = async (copydir, distdir) => {
+  try {
+    const files = await readdir(copydir, { withFileTypes: true });
+    for (const file of files) {
+      if (file.isDirectory()) {
+        const dist = path.join(distdir, file.name);
+        await mkdir(dist, { recursive: true });
+      }
+      if (file.isFile()) {
+        const filename = path.join(copydir, file.name);
+        const copyfilename = path.join(distdir, file.name);
+        await copyFile(filename, copyfilename);
+      }
+    }
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const readcontent = async (filename, number) => {
+  const file = new fs.ReadStream(filename, 'utf8');
+
+  file.on('data', (message) => {
+    if (message != undefined) {
+      content[number] += message;
+    }
+  });
+  file.on('error', (error) => console.log('Error: ', error));
+  file.on('end', () => {
+    file.destroy;
+    stylesfile.write(content[number]);
+  });
+};
+
+const copystyle = async () => {
+  try {
+    const files = await readdir(stylesdir, { withFileTypes: true });
+    let number = 0;
+    for (const file of files) {
+      if (file.isFile()) {
+        const filename = path.join(stylesdir, file.name);
+        const detail = path.parse(file.name);
+        if (detail.ext === '.css') {
+          await readcontent(filename, number);
+          content[number] = '';
+          number++;
+        }
+      }
+    }
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const createindex = async () => {
+  let content = { index: '', header: '', articles: '', footer: '' };
+  const componentsdir = path.join(__dirname, 'components');
+  const files = await readdir(componentsdir, { withFileTypes: true });
+  for (const file of files) {
+    if (file.isFile()) {
+      const filename = path.join(componentsdir, file.name);
+      const readfile = new fs.ReadStream(filename, 'utf8');
+      readfile.on('data', (message) => {
+        switch (file.name) {
+          case 'articles.html':
+            content.articles += message;
+            break;
+          case 'footer.html':
+            content.footer += message;
+            break;
+          case 'header.html':
+            content.header += message;
+            break;
+        }
+      });
+      readfile.on('error', (error) => console.log('Error: ', error));
+      readfile.on('end', () => {
+        readfile.destroy;
+      });
+    }
+  }
+  const filename = path.join(__dirname, 'template.html');
+  const file = new fs.ReadStream(filename, 'utf8');
+  file.on('data', (message) => {
+    content.index += message;
+  });
+  file.on('error', (error) => console.log('Error: ', error));
+  file.on('end', () => {
+    file.destroy;
+    content.index = content.index.replace('{{header}}', content.header);
+    content.index = content.index.replace('{{articles}}', content.articles);
+    content.index = content.index.replace('{{footer}}', content.footer);
+    const indexdist = path.join(distdir, 'index.html');
+    const indexfile = new fs.WriteStream(indexdist, 'utf8');
+    indexfile.write(content.index);
+  });
+};
+
+createdist();
+copystyle();
+createindex();
