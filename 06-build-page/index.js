f493f6b13ae74fba75b4e73c813a10f10bebd648
Oleg S <oleg.sl62@mail.ru>
2022-05-24 11:50:48 +0300

    fix: correct  assembly of index.html

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -9,6 +9,7 @@ const distdir = path.join(__dirname, 'project-dist');
 const stylesfilename = path.join(distdir, 'style.css');
 const stylesfile = new fs.WriteStream(stylesfilename, 'utf8');
 let content = [];
+let indexpart = [];
 
 const createdist = async () => {
   try {
@@ -82,24 +83,21 @@ const copystyle = async () => {
 };
 
 const createindex = async () => {
-  let content = { index: '', header: '', articles: '', footer: '' };
+  indexpart.push({ tag: 'index', body: ' ' });
   const componentsdir = path.join(__dirname, 'components');
   const files = await readdir(componentsdir, { withFileTypes: true });
   for (const file of files) {
     if (file.isFile()) {
       const filename = path.join(componentsdir, file.name);
+      const fname = path.parse(file.name);
+      indexpart.push({ tag: fname.name, body: '' });
       const readfile = new fs.ReadStream(filename, 'utf8');
       readfile.on('data', (message) => {
-        switch (file.name) {
-          case 'articles.html':
-            content.articles += message;
-            break;
-          case 'footer.html':
-            content.footer += message;
-            break;
-          case 'header.html':
-            content.header += message;
-            break;
+        const name = path.parse(file.name);
+        for (const item of indexpart) {
+          if (item.tag == name.name) {
+            item.body += message;
+          }
         }
       });
       readfile.on('error', (error) => console.log('Error: ', error));
@@ -108,20 +106,25 @@ const createindex = async () => {
       });
     }
   }
+
   const filename = path.join(__dirname, 'template.html');
   const file = new fs.ReadStream(filename, 'utf8');
   file.on('data', (message) => {
-    content.index += message;
+    indexpart[0].body += message;
   });
   file.on('error', (error) => console.log('Error: ', error));
   file.on('end', () => {
     file.destroy;
-    content.index = content.index.replace('{{header}}', content.header);
-    content.index = content.index.replace('{{articles}}', content.articles);
-    content.index = content.index.replace('{{footer}}', content.footer);
+    for (const item of indexpart) {
+      if (item != 'index') {
+        let tag = '{{' + item.tag + '}}';
+        indexpart[0].body = indexpart[0].body.replace(tag, item.body);
+      }
+    }
     const indexdist = path.join(distdir, 'index.html');
     const indexfile = new fs.WriteStream(indexdist, 'utf8');
-    indexfile.write(content.index);
+    indexfile.write(indexpart[0].body);
+    console.log(indexpart[0].body);
   });
 };
 
